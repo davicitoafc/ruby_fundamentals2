867b3e7dac76eeb461718b656669a7d9de907188
David Cisneros <d.cisneros849@gmail.com>
2016-06-01 16:39:21 -0400

    Added a list

--- exercise6.rb
+++ exercise6.rb
@@ -1,16 +1,12 @@
 
 
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-
-
-grocery_list.map { |item| puts "*" + item }
-
-grocery_list << "rice"
-
-
 def list(grocery_list)
-  p grocery_list
-  return
+  grocery_list.each do |item|
+    puts "*" + item
+  end
 end
 
-list
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+grocery_list << "rice"
+
+list(grocery_list)
