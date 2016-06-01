ff15c6511b4ca430807da484c67fc799a143c6d3
David Cisneros <d.cisneros849@gmail.com>
2016-06-01 17:13:59 -0400

    showed second indice in grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -9,13 +9,14 @@ end
 def search(grocery_list)
   if grocery_list.include?("bananas")
     puts "You need to pick up bananas"
-  else puts "You don't need to pick up bananas today"
+  else
+    puts "You don't need to pick up bananas today"
   end
 end
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 grocery_list << "rice"
 puts grocery_list.count
-
+puts grocery_list[1]
 list(grocery_list)
 search(grocery_list)
