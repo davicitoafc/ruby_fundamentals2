a0fdd6344de8c6d46ada47804ad75aa07b9548c6
David Cisneros <d.cisneros849@gmail.com>
2016-06-02 14:46:24 -0400

    corrected expansion method and created variable for readible code

--- exercise7.rb
+++ exercise7.rb
@@ -10,8 +10,27 @@ def hash(students)
   students.each do |key, value|
     puts "#{key}: #{value} students"
   end
+  puts " "
+end
+
+#expansion method iterates through student hash and adds 5% to each class
+def expansion(students)
+  students.each do |key, value|
+    expanded = (value + (value * 0.05).floor)
+    puts "#{key}: #{expanded}"
+  end
+  puts " "
 end
 
 #Prints cohort names and adds cohort4 number to hash
+puts "Cohort List"
 students[:cohort4] = 43
 hash(students)
+
+#Keys method to show to output cohort names
+puts "New Cohort list"
+hash(students.keys)
+
+#Prints out expansion
+puts "Expanded Cohort list"
+expansion(students)
