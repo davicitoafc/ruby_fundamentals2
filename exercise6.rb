2cff23273dd36011eb928492d05e691c67977c33
David Cisneros <d.cisneros849@gmail.com>
2016-06-02 00:22:06 -0400

    Organized code with descriptions and deleted salmon from grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,4 @@
-
+#Created a list print method to add an * to each item on grocery_list
 
 def list(grocery_list)
   grocery_list.each do |item|
@@ -6,18 +6,45 @@ def list(grocery_list)
   end
 end
 
+#Created search method to determine if bananas were on the grocery_list
+
 def search(grocery_list)
   if grocery_list.include?("bananas")
     puts "You need to pick up bananas"
   else
     puts "You don't need to pick up bananas today"
+    puts " "
   end
 end
 
+#grocery_list array
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+#added rice to grocery_list
 grocery_list << "rice"
+
+#Counted grocery_list items
 puts grocery_list.count
+
+#Shows item on indices 1 of grocery_list array
 puts grocery_list[1]
-list(grocery_list)
-search(grocery_list)
+puts " "
+
+#Sorts grocery_list alphabetically
+puts "Sorted list"
 list(grocery_list.sort)
+puts " "
+
+#Prints out grocery_list
+puts "Grocery List"
+list(grocery_list)
+puts " "
+
+#Prints output for search method
+search(grocery_list)
+puts " "
+
+#removes item at index 3
+puts "Shorter list"
+grocery_list.delete("salmon")
+list(grocery_list)
